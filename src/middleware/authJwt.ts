46139a568b164c6e6c4d2888aa870ec76f70f35b
Fabien Winkler <fabien.winkler@outlook.fr>
2022-03-05 00:17:17 +0100

    [MàJ] modules

--- src/middleware/authJwt.ts
+++ src/middleware/authJwt.ts
@@ -1,6 +1,6 @@
 import { NextFunction, Request, Response } from 'express';
 import jwt from 'jsonwebtoken';
-import { TokenExpiredError } from 'jsonwebtoken'
+import { TokenExpiredError } from 'jsonwebtoken';
 /**
  * @param {Request} req
  * @param {Response} res
